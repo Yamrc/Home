b418d9c5d2501e825e4016f9c6d55a11671c8bfa
Yamrc <yamrci@outlook.com>
2025-10-11 21:30:29 +0800

    Feat(App): 初始化项目
    - 采用SolidJS+UnoCSS

---
+++ unocss.config.ts
@@ -0,0 +1,11 @@
+import { defineConfig } from 'unocss'
+import { presetWind3 } from '@unocss/preset-wind3'
+
+export default defineConfig({
+  presets: [presetWind3()],
+  preflights: [
+    {
+      getCSS: () => 'body{margin:0}',
+    },
+  ],
+})
---
+++ vite.config.ts
@@ -0,0 +1,7 @@
+import { defineConfig } from 'vite'
+import solid from 'vite-plugin-solid'
+import Unocss from 'unocss/vite'
+
+export default defineConfig({
+    plugins: [solid(), Unocss()],
+})
